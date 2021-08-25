5ac93209e782de7a233acc9ea51dd47532c05e3a
baiquan <2623085875@qq.com>
2021-08-25 11:33:32 +0800

    BiJi分支修改Main

--- app/src/main/java/com/bq/mya/MainActivity.java
+++ app/src/main/java/com/bq/mya/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //台式机第一次提交
+        //笔记本第一次提交
     }
 }
